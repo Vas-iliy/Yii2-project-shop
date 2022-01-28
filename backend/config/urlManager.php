91ed9273a3fc3c5e088555de3f483b869194e37a
Vasiliy <kkolyasev1228@gmail.com>
2022-01-28 17:19:00 +0400

    url

---
+++ backend/config/urlManager.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'class' => 'yii\web\UrlManager',
+    'hostInfo' => 'frontendHostInfo',
+
+    'enablePrettyUrl' => true,
+    'showScriptName' => false,
+    'rules' => [
+        '' => 'site/index',
+        '<_a:logout|login>' => 'site/<_a>',
+
+        '<_c:[\w\-]+>' => '<_c>/index',
+        '<_c:[\w\-]+>/<id:\d+>' => '<_c>/view',
+        '<_c:[\w\-]+>/<_a:[\w-]+>' => '<_c>/<_a>',
+        '<_c:[\w\-]+>/<id:\d+>/<_a:[\w\-]+>' => '<_c>/<_a>',
+    ],
+];
--- common/config/params.php
+++ common/config/params.php
@@ -6,4 +6,7 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
+    'cookieDomain' => '..shop',
+    'frontendHostInfo' => 'http://shop',
+    'backendHostInfo' => 'http://shop/adminpanel',
 ];
