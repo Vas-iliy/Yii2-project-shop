7e9974b29faab37e2c78d5335c6a9304d651fc42
Vasiliy <kkolyasev1228@gmail.com>
2022-01-29 15:53:37 +0400

    admin lte

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,12 +12,19 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+        'css/font-awesome.min.css',
+        'css/ionicons.min.css',
+        'css/AdminLTE.min.css',
+        'css/skin-blue.min.css',
+        'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic',
     ];
     public $js = [
+        'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js',
+        'https://oss.maxcdn.com/respond/1.4.2/respond.min.js',
+        'js/adminlte.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap4\BootstrapAsset',
+        'yii\bootstrap4\BootstrapPluginAsset',
     ];
 }
