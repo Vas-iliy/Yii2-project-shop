1fcd3156a1fd00168070cc3cdf34fd4de529ef1f
Vasiliy <kkolyasev1228@gmail.com>
2022-02-04 12:55:43 +0400

    meta

--- shop/entities/behaviors/MetaBehavior.php
+++ shop/entities/behaviors/MetaBehavior.php
@@ -6,13 +6,15 @@ use shop\entities\Meta;
 use yii\base\Behavior;
 use yii\base\Event;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Json;
 
 class MetaBehavior extends Behavior
 {
     public $attribute = 'meta';
     public $jsonAttribute = 'meta_json';
-    public function events()
+
+    public function events(): array
     {
         return [
             ActiveRecord::EVENT_AFTER_FIND => 'onAfterFind',
@@ -21,20 +23,24 @@ class MetaBehavior extends Behavior
         ];
     }
 
-    public function onAfterFind(Event $event)
+    public function onAfterFind(Event $event): void
     {
-        $brand = $event->sender;
-        $meta = Json::decode($brand->getAttribute($this->jsonAttribute));
-        $brand->{$this->attribute} = new Meta($meta['title'] ?? null, $meta['description'] ?? null, $meta['keywords'] ?? null);
+        $model = $event->sender;
+        $meta = Json::decode($model->getAttribute($this->jsonAttribute));
+        $model->{$this->attribute} = new Meta(
+            ArrayHelper::getValue($meta, 'title'),
+            ArrayHelper::getValue($meta, 'description'),
+            ArrayHelper::getValue($meta, 'keywords')
+        );
     }
 
-    public function onBeforeSave(Event $event)
+    public function onBeforeSave(Event $event): void
     {
-        $brand = $event->sender;
-        $brand->setAttribute($this->jsonAttribute, Json::encode([
-            'title' => $brand->{$this->attribute}->title,
-            'description' => $brand->{$this->attribute}->description,
-            'keywords' => $brand->{$this->attribute}->keywords,
+        $model = $event->sender;
+        $model->setAttribute('meta_json', Json::encode([
+            'title' => $model->{$this->attribute}->title,
+            'description' => $model->{$this->attribute}->description,
+            'keywords' => $model->{$this->attribute}->keywords,
         ]));
     }
 }
