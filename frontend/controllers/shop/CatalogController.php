a9c43300193dbbbc5cc530452e8c00f21ad929a5
Vasiliy <kkolyasev1228@gmail.com>
2022-03-14 15:09:43 +0400

    action index

--- frontend/controllers/shop/CatalogController.php
+++ frontend/controllers/shop/CatalogController.php
@@ -2,15 +2,42 @@
 
 namespace frontend\controllers\shop;
 
+use shop\readModels\Shop\BrandReadRepository;
+use shop\readModels\Shop\CategoryReadRepository;
+use shop\readModels\Shop\ProductReadRepository;
+use shop\readModels\Shop\TagReadRepository;
 use yii\web\Controller;
 
 class CatalogController extends Controller
 {
     public $layout = 'catalog';
 
+    private $products;
+    private $categories;
+    private $brands;
+    private $tags;
+
+    public function __construct($id, $module,
+                                ProductReadRepository $products,
+                                CategoryReadRepository $categories,
+                                BrandReadRepository $brands,
+                                TagReadRepository $tags, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->products = $products;
+        $this->categories = $categories;
+        $this->brands = $brands;
+        $this->tags = $tags;
+    }
+
     public function actionIndex()
     {
-        return $this->render('index');
+        $dataProvider = $this->products->getAll();
+        $category = $this->categories->getRoot();
+        return $this->render('index', [
+            'category' => $category,
+            'dataProvider' => $dataProvider
+        ]);
     }
 
     public function actionCategory($id)
