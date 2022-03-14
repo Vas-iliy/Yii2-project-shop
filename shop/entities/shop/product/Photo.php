2eed7ab74f78d8990d9dfce5643e3c800cd0e065
Vasiliy <kkolyasev1228@gmail.com>
2022-03-14 18:12:23 +0400

    refactor photo product

--- shop/entities/shop/product/Photo.php
+++ shop/entities/shop/product/Photo.php
@@ -2,6 +2,7 @@
 
 namespace shop\entities\shop\product;
 
+use shop\services\WaterMarker;
 use yii\db\ActiveRecord;
 use yii\web\UploadedFile;
 use yiidreamteam\upload\ImageUploadBehavior;
@@ -54,12 +55,9 @@ class Photo extends ActiveRecord
                     'cart_list' => ['width' => 150, 'height' => 150],
                     'cart_widget_list' => ['width' => 57, 'height' => 57],
                     'catalog_list' => ['width' => 228, 'height' => 228],
-                    /*'catalog_product_main' => ['processor' => [new WaterMarker(750, 1000, '@frontend/web/image/logo.png'), 'process']],
+                    'catalog_product_main' => ['processor' => [new WaterMarker(750, 1000, '@frontend/web/image/logo.png'), 'process']],
                     'catalog_product_additional' => ['width' => 66, 'height' => 66],
-                    'catalog_origin' => ['processor' => [new WaterMarker(1024, 768, '@frontend/web/image/logo.png'), 'process']],*/
-                    'catalog_product_main' => ['width' => 750, 'height' => 1000],
-                    'catalog_product_additional' => ['width' => 66, 'height' => 66],
-                    'catalog_origin' => ['width' => 1024, 'height' => 768],
+                    'catalog_origin' => ['processor' => [new WaterMarker(1024, 768, '@frontend/web/image/logo.png'), 'process']],
                 ],
             ],
         ];
